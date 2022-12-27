b3d76763884daf3d79ef5e0892d85ddddcde13e9
DevilSpiderX <775716441@qq.com>
2022-12-28 02:22:40 +0800

    全局样式提出到单独文件

--- src/App.vue
+++ src/App.vue
@@ -33,31 +33,3 @@ setInterval(checkUserStatus, appConfigs.user.checkIntervalTime);
 <template>
     <RouterView />
 </template>
-
-<style>
-@import url(./styles/box-shadow.css);
-
-html,
-body,
-div#app {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-}
-
-body {
-    font-family: "MiSans Normal" !important;
-    background-color: var(--color-bg-1);
-    color: var(--color-text-1);
-}
-
-@font-face {
-    font-family: "MiSans Normal";
-    src: url("fonts/MiSans-Normal.ttf");
-}
-
-body.no-scrollbar::-webkit-scrollbar {
-    display: none;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "./style.css";
+import "./styles/box-shadow.css";
 import router from './router/router';
 import pinia from './store/pinia';
 
