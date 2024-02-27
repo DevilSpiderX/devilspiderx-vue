977024fb85f20cf196f0166a58d5916f669c46f8
DevilSpiderX <775716441@qq.com>
2024-02-27 23:53:26 +0800

    1.更新ANi模块中的getXML接口参数

--- src/components/ANi/scripts/api.ts
+++ src/components/ANi/scripts/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export async function getXML() {
-    const resp = await axios.get<string>("/api/ani/torrent");
+    const resp = await axios.get<string>("/api/ani/torrent", {
+        params: {
+            proxy: true
+        }
+    });
     return resp.data;
 }
